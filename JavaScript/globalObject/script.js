2f82513e6a4d96cad9342cee5f81b3f6f92eb67b
Bhaskar <bhaskar9.koilada@gmail.com>
2021-04-11 20:43:42 +0530

    globalObject

---
+++ JavaScript/globalObject/script.js
@@ -0,0 +1,8 @@
+function sayHi(){
+    console.log("hi")
+    }
+console.log(sayHi.name);
+function fun (game,ball){}
+function ran(aa,ff,a,...more){}
+console.log(fun.length);
+console.log(ran.length);
